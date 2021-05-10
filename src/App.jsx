8627da7068e0ea19d9f2848bf19569ce8c2ad9cd
Luiz Armando Galesso <luiz.galesso@b2wdigital.com>
2021-05-10 20:50:23 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,6 @@ import Card from './components/Card.jsx'
 
 export default props => (
   <div>
-    <Card Title="Cloud Development" color="#9C0F5F">Nomes e RMs</Card>
+    <Card Title="Cloud Development" color="#9C0F5F">Teste</Card>
   </div>
 )
