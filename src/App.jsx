2c2e160b9817f1841f7ddff7b529c04088aa55b5
Luiz Armando Galesso <luiz.galesso@b2wdigital.com>
2021-05-16 16:30:30 -0300

    Versao final

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,16 @@ import Card from './components/Card.jsx'
 
 export default props => (
   <div>
-    <Card Title="Cloud Development" color="#9C0F5F">Teste</Card>
+    <Card Title="Cloud Development" color="#9C0F5F">
+      HAROLD ISAAC CAMPERO STOFFERS MURILLO <br/> 
+      Rm: 337233<br/>
+      LEONARDO VILLANI <br/>
+      Rm: 337936<br/>
+      LUIZ ARMANDO GALESSO <br/>
+      Rm: 338089 <br/>
+      <br/>
+      Repositório: <br/>
+      <a href="https://github.com/lag1985/clouddevelopment">https://github.com/lag1985/clouddevelopment</a>
+    </Card>
   </div>
 )
